889c466aa8059a41d3740971da814bd43b5671a3
valid <valid@admin>
2023-03-17 15:40:49 +0530

    Create a Book class and Test It

---
+++ Test/com/book/details/BookTest.java
@@ -0,0 +1,69 @@
+package com.book.details;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookTest {
+
+	@Test
+	public void testBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPublisher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetYear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPublisher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShow() {
+		fail("Not yet implemented");
+	}
+
+}
